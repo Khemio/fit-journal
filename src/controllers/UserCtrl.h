8db9cd37c2129d935e1ba6161d61fa4b2504b714
Zaikin Andrei <inerizal@gmail.com>
2026-04-08 15:08:37 +0200

    cleanup: comments

--- src/controllers/UserCtrl.h
+++ src/controllers/UserCtrl.h
@@ -4,25 +4,21 @@
 
 using namespace drogon;
 
-//TODO: Add User Signup
 //TODO: Remove users
 
 class Users : public drogon::HttpController<Users>
 {
     public:
         METHOD_LIST_BEGIN
-        // use METHOD_ADD to add your custom processing function here;
         METHOD_ADD(Users::Auth, "/auth", Get);  //? Consider better flow for auth
         METHOD_ADD(Users::SignUp, "/signup", Post);
         METHOD_ADD(Users::SignIn, "/signin", Post);
         METHOD_ADD(Users::SignOut, "/signout", Post);
         // METHOD_ADD(Users::DeleteUser, "/delete", Delete);
-
         METHOD_LIST_END
-        // your declaration of processing function maybe like this:
+
         void Auth(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
         void SignOut(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
-        // void SignIn(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
         Task<HttpResponsePtr> SignUp(HttpRequestPtr req);
         Task<HttpResponsePtr> SignIn(HttpRequestPtr req);
         // Task<HttpResponsePtr> DeleteUser(HttpRequestPtr req);
