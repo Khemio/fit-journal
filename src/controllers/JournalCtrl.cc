f9060253315ffe9c8b83233ea30a4961ca12c15a
Zaikin Andrei <inerizal@gmail.com>
2026-04-12 23:46:23 +0200

    feat: list functionaity for journals

--- src/controllers/JournalCtrl.cc
+++ src/controllers/JournalCtrl.cc
@@ -3,3 +3,30 @@
 #include "../models/journal.h"
 
 // Add definition of your processing function here
+Task<HttpResponsePtr> Journals::get_all(HttpRequestPtr req) {
+    // HttpResponsePtr resp;
+    HttpViewData data;
+    std::vector<Journal> jrs;
+
+    auto user_id = req->session()->getOptional<unsigned long>("ID").value_or(0);
+    auto client = app().getDbClient();
+    auto result = co_await client->execSqlCoro("SELECT * FROM journals WHERE owner_id = ?;", user_id);
+
+    for (auto row : result) {
+        Journal jr{
+            .ID = row[0].as<unsigned long>(),
+            .owner_id = row[1].as<unsigned long>(),
+            .name = row[2].as<std::string>(),
+            .type = row[3].as<std::string>(),
+            .description = row[4].as<std::string>()
+        };
+
+        jrs.push_back(jr);
+    }
+
+    data.insert("journals", jrs);
+
+    auto resp = HttpResponse::newHttpViewResponse("journals", data);
+
+    co_return resp;
+}
--- src/controllers/JournalCtrl.h
+++ src/controllers/JournalCtrl.h
@@ -8,21 +8,22 @@ using namespace drogon;
 //TODO: Show one journal
 //TODO: Add journal
 //TODO: Remove journal?
+//? Might be a good idea to limit amount of journals
 
 class Journals : public drogon::HttpController<Journals>
 {
     public:
         METHOD_LIST_BEGIN
         // use METHOD_ADD to add your custom processing function here;
-        // METHOD_ADD(Journals::get_all, "", Get);
-        // METHOD_ADD(Journals::get_all, "/", Get);
-        // METHOD_ADD(Journals::get_some, "/search?food_name={1}", Get, Post);
-        // METHOD_ADD(Journals::get_some, "/{1}", Get);
-        // METHOD_ADD(Journals::get_one, "/add/{}", Get);
+        METHOD_ADD(Journals::get_all, "", Get, "AuthFilter");
+        METHOD_ADD(Journals::get_all, "/", Get, "AuthFilter");
+        // METHOD_ADD(Journals::get_one, "/{}", Get, "AuthFilter");
+        // METHOD_ADD(Journals::create_new, "/new", Post, "AuthFilter");
+        // METHOD_ADD(Journals::delete_one, "/{}/delete", Delete, "AuthFilter");
 
         METHOD_LIST_END
         // your declaration of processing function maybe like this:
-        // Task<HttpResponsePtr> get_all(HttpRequestPtr req);
+        Task<HttpResponsePtr> get_all(HttpRequestPtr req);
         // Task<HttpResponsePtr> get_some(HttpRequestPtr req, std::string &&name);
         // Task<HttpResponsePtr> get_one(HttpRequestPtr req, unsigned long &&food_id);
 };
