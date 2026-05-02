eff099a24763b3551822c6555017c0863f424828
Zaikin Andrei <inerizal@gmail.com>
2026-05-02 21:23:01 +0200

    refactor: food ctrl no uses db-manager

--- src/controllers/FoodCtrl.cc
+++ src/controllers/FoodCtrl.cc
@@ -3,39 +3,40 @@
 #include "../dto/food.h"
 #include "../database/DbManager.h"
 
-
-Task<HttpResponsePtr> Foods::get_all(HttpRequestPtr req) {
-    auto fds = co_await DbManager::get_all_foods();
+HttpResponsePtr render_food(std::string page, bool isFinal, std::vector<Food> fds) {
     HttpViewData data;
 
-    data.insert("isFinal", false);
+    data.insert("isFinal", isFinal);
     data.insert("foods", fds);
 
-    auto resp = HttpResponse::newHttpViewResponse("FoodPage", data);
+    return HttpResponse::newHttpViewResponse(page, data);
+}
+
+HttpResponsePtr render_food_page(bool isFinal, std::vector<Food> fds) {
+    return render_food("FoodPage", isFinal, fds);
+}
+
+HttpResponsePtr render_food_list(bool isFinal, std::vector<Food> fds) {
+    return render_food("FoodList", isFinal, fds);
+}
+
+Task<HttpResponsePtr> Foods::get_all(HttpRequestPtr req) {
+    auto fds = co_await DbManager::get_instance()->get_all_foods();
+    auto resp = render_food_page(false, fds);
 
     co_return resp;
 }
 
 Task<HttpResponsePtr> Foods::get_some(HttpRequestPtr req, std::string &&name) {
-    auto fds = co_await DbManager::get_foods_by_name(name);
-    HttpViewData data;
-
-    data.insert("isFinal", false);
-    data.insert("foods", fds);
-
-    auto resp = HttpResponse::newHttpViewResponse("FoodList", data);
+    auto fds = co_await DbManager::get_instance()->get_foods_by_name(name);
+    auto resp = render_food_list(false, fds);
 
     co_return resp;
 }
 
 Task<HttpResponsePtr> Foods::get_one(HttpRequestPtr req, unsigned long &&food_id) {
-    auto fds = co_await DbManager::get_food_by_id(food_id);
-    HttpViewData data;
-
-    data.insert("isFinal", true);
-    data.insert("foods", fds);
-    
-    auto resp = HttpResponse::newHttpViewResponse("FoodList", data);
+    auto fds = co_await DbManager::get_instance()->get_food_by_id(food_id);
+    auto resp = render_food_list(true, fds);
 
     co_return resp;
 }
--- src/database/DbManager.h
+++ src/database/DbManager.h
@@ -1,24 +1,34 @@
 #pragma once
 #include <drogon/drogon.h>
 
-// #include "../dto/user.h"
 #include "../dto/food.h"
 
 using namespace drogon::orm;
 
 class DbManager {
     public:
+        DbManager(DbManager &other) = delete;
+        void operator=(const DbManager &other) = delete;
+        static DbManager* get_instance();
 
-        static Task<std::vector<Food>> get_all_foods();
-        static Task<std::vector<Food>> get_foods_by_name(std::string &name);
-        static Task<std::vector<Food>> get_food_by_id(unsigned long &food_id);
-
-        // static Task<void> add_user(std::string user, std::string hash);
-        // static Task<User> get_user(std::string user);   
-
+        Task<std::vector<Food>> get_all_foods();
+        Task<std::vector<Food>> get_foods_by_name(std::string &name);
+        Task<std::vector<Food>> get_food_by_id(unsigned long &food_id);
     private:
+        static DbManager* manager;
+        DbManager() {};
+
         static Task<Result> get_from_foods_table() ;
 };
+
+DbManager* DbManager::manager = nullptr;
+
+DbManager* DbManager::get_instance() {
+    if (manager == nullptr) {
+        manager = new DbManager();
+    }
+    return manager;
+}
 
 Task<Result> DbManager::get_from_foods_table() {
     auto client = app().getDbClient();
@@ -66,23 +76,3 @@ Task<std::vector<Food>> DbManager::get_food_by_id(unsigned long &food_id) {
 
     co_return fds;
 }
-
-// Task<void> DbManager::add_user(std::string user, std::string hash) {
-//     auto client = app().getDbClient();
-//     co_await client->execSqlCoro(
-//         "INSERT INTO users (username, password) VALUES ($1, $2);", 
-//         user, hash);
-// }
-
-// Task<User> DbManager::get_user(std::string user) {
-//     auto client = app().getDbClient();
-//     auto result = co_await client->execSqlCoro("SELECT * FROM users WHERE username = ?;", user);
-
-//     if (result.size() > 0) { //? Size probably should be 1 for a singular row
-//         User user{result[0]};
-//         co_return user;
-//     } else {
-//         co_return User{};
-//     }
-
-// }
