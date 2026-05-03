87c4a1615694c08e3420f93b141f3edcc8a54665
Zaikin Andrei <inerizal@gmail.com>
2026-05-03 23:41:23 +0200

    refactor: added auth service factory

--- src/filters/AuthFilter.cc
+++ src/filters/AuthFilter.cc
@@ -2,19 +2,48 @@
 
 using namespace drogon;
 
+class AuthService {
+    public:
+        virtual void authenticate() {};
+        virtual void authorize() {};
+};
+
+class SessionAuthService : public AuthService {
+    public:
+        SessionAuthService(const HttpRequestPtr req) : req(req) {};
+        void authenticate() override {
+            isAuthenticated =  req->session()->getOptional<bool>("loggedIn").value_or(false);
+        }
+        virtual void authorize() override {isValid = isAuthenticated;};
+
+        bool isValid;
+
+    private:
+        bool isAuthenticated;
+        const HttpRequestPtr req;
+};
+
+class AuthServiceFactory {
+    public:
+        static SessionAuthService getSessionAuthService(const HttpRequestPtr req) {
+            return SessionAuthService{req};
+        };
+};
+
 void AuthFilter::doFilter(const HttpRequestPtr &req,
                          FilterCallback &&fcb,
                          FilterChainCallback &&fccb)
 {
-    bool loggedIn = req->session()->getOptional<bool>("loggedIn").value_or(false);
-    if (loggedIn)
+    auto auth = AuthServiceFactory::getSessionAuthService(req);
+    auth.authenticate();
+    auth.authorize();
+    if (auth.isValid)
     {
         fccb();
         return;
     }
     HttpResponsePtr resp = HttpResponse::newHttpResponse();
     resp->setStatusCode(k401Unauthorized);
-    // resp->setBody("<script>window.location.href = \"/\";</script>");
     resp->addHeader("HX-Location", "/");
     
     fcb(resp);
